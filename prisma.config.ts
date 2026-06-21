ddefae95f04a48f26e797a6665df8b6ccdad0f6c
Reno Febriyanto <renofebriyanto94@gmail.com>
2026-06-21 17:16:14 +0700

    Update prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -4,9 +4,8 @@ import * as path from 'path'
 
 dotenv.config({ path: path.resolve(process.cwd(), '.env.local') })
 dotenv.config({ path: path.resolve(process.cwd(), '.env') })
-
 export default defineConfig({
   datasource: {
-    url: process.env.DIRECT_URL!,
+    url: process.env.DIRECT_URL!,  // pakai direct, bukan pooler
   },
 })
